1bae4d2f6f80a09bc2c0624d63f861a9ef8b0697
Katherine Negrete Aguilar <katherine@vinneren.com.mx>
2023-01-04 14:33:55 -0600

    Updates

---
+++ precache-manifest.2883dd52fc99ddc5d9ad29757a0a0e30.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "65885f0897c4aa8e3d49",
+    "url": "/daily/static/css/main.7ca9fb1b.chunk.css"
+  },
+  {
+    "revision": "65885f0897c4aa8e3d49",
+    "url": "/daily/static/js/main.65885f08.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/daily/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/daily/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "9ee5528ca23b07b91f69",
+    "url": "/daily/static/js/runtime~main.9ee5528c.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/daily/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "534b9d03bf264ce24c29d57ba69b7f1b",
+    "url": "/daily/index.html"
+  }
+];
